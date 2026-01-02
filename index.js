feac2ddf8ee08efa6221b5bdf5e87d9a792ea1f5
devthedevloper <110390531+anonymous6419@users.noreply.github.com>
2026-01-02 12:38:30 +0530

    testing

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+
+console.log("LocationId", process.env.LOCATION);
+console.log("ProjectId", process.env.PROJECT_ID);
+console.log("VERTEX_AI_CREDENTIALS_BASE64", process.env.VERTEX_AI_CREDENTIALS_BASE64);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
